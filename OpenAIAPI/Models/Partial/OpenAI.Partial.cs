54400f81d3fe953e48657a5419fd417c047d84f9
f137941 <momochenisme@gmail.com>
2023-06-16 17:47:32 +0800

    添加Function Calling相關Model

--- OpenAIAPI/Models/Partial/OpenAI.Partial.cs
+++ OpenAIAPI/Models/Partial/OpenAI.Partial.cs
@@ -1,4 +1,6 @@
-﻿namespace OpenAIAPI.Models.Partial
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace OpenAIAPI.Models.Partial
 {
     public enum QAType
     {
@@ -38,6 +40,12 @@
         public bool stream { get; set; }
     }
 
+    public class ChatGPTFunctionCallRequestModel : ChatGPTRequestModel
+    {
+        public List<FunctionCallModel> functions { get; set; }
+        public dynamic function_call { get; set; }
+    }
+
     public class ChatGPTMessageModel
     {
         public string role { get; set; }
@@ -69,5 +77,31 @@
         public string prompt { get; set; }
 
         public List<UsingTextModel> usingText { get; set; }
+    }
+
+    public class FunctionCallModel
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public ParamterModel parameters { get; set; }
+    }
+
+    public class ParamterModel
+    {
+        public string type { get; set; }
+        public Dictionary<string, PropertyModel> properties { get; set; }
+        public List<string>? required { get; set; }
+    }
+
+    public class PropertyModel
+    {
+        public string type { get; set; }
+        public string? description { get; set; }
+        public ParamterModel items { get; set; }
+    }
+
+    public class QAFunctionCallResponseModel
+    {
+        public List<UsingTextModel> data { get; set; }
     }
 }
